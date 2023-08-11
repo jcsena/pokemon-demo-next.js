c3ff28ef5ca19d15cbae7565cad07eae0dc0a1c4
Julio Sena <jsena@assanet.com>
2023-08-11 08:00:18 -0500

    new component

--- components/pages/HomePage/HomePage.tsx
+++ components/pages/HomePage/HomePage.tsx
@@ -1,21 +1,29 @@
-import DefaultTemplate from '@/components/templates/DefaultTemplate';
-import ItemList from '@/components/UI/organisms/ItemList';
-import PokemonList from '@/lib/types';
+import DefaultTemplate from "@/components/templates/DefaultTemplate";
+import ItemList from "@/components/UI/organisms/ItemList";
+import PokemonList from "@/lib/types";
 import {
   IMG_ALT_KEY,
   IMG_URL_KEY,
   LINK_PATH_KEY,
-} from '@/utils/mapPokemonPaginatedResults';
-import { InfiniteData } from '@tanstack/react-query';
-import { FC } from 'react';
+} from "@/utils/mapPokemonPaginatedResults";
+import { InfiniteData } from "@tanstack/react-query";
+import dynamic from "next/dynamic";
+import { FC } from "react";
 
 interface Props {
   pokemonList: InfiniteData<PokemonList>;
 }
 
+const TextLabel = dynamic(() =>
+  import("../../UI/atoms/TextLabel").then(
+    (IndexComponent) => IndexComponent.TextLabel
+  )
+);
+
 const HomePage: FC<Props> = ({ pokemonList }): JSX.Element => {
   return (
     <DefaultTemplate title="NextJS Pokemon Search App">
+      <TextLabel>Locuras puras</TextLabel>
       <ItemList
         list={pokemonList}
         titleKey="name"
